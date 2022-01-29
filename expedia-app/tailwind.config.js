6d34436ff8ffb331b7259cadecac5a2cf53f4de1
risako <lilisako062397@gmail.com>
2022-01-29 20:10:16 +0900

    feat: add expedia-app

---
+++ expedia-app/tailwind.config.js
@@ -0,0 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    fontFamily: {
+      google: ['Roboto'],
+    },
+    extend: {
+      colors: {
+        orange: colors.orange
+      }
+    }
+  }
+};

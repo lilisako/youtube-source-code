c598d42ec0bbee05dbd4ebc5e3977a78f08b1e54
risako <lilisako062397@gmail.com>
2022-01-30 13:59:11 +0900

    feat: add twitter simple button

---
+++ tailwind-tutorial-for-beginner/pages/button.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="m-4">
+    <button class="bg-twitter-blue hover:bg-twitter-dark-blue text-white font-bold px-4 py-2 rounded-full">Tweet</button>
+  </div>
+</template>
---
+++ tailwind-tutorial-for-beginner/tailwind.config.js
@@ -0,0 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'twitter-blue': '#1C9BEF',
+        'twitter-dark-blue': '#188CD8'
+      }
+    }
+  }
+};

91c1a951e9b4158f42df527a10b87def77f06661
risako <lilisako062397@gmail.com>
2022-01-31 19:28:06 +0900

    feat: add simple card

---
+++ tailwind-tutorial-for-beginner/pages/simple-card.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex items-center m-2">
+    <!-- image component -->
+    <div>
+      <img src="https://tailwind.risako.dev/rome.png" class="w-16 h-16 rounded-md" />
+    </div>
+    <!-- text component -->
+    <div class="mx-2">
+      <p class="font-bold">7 best places you should visit in Rome</p>
+      <p class="text-xs text-gray-400">6 hours ago</p>
+    </div>
+  </div>
+</template>
